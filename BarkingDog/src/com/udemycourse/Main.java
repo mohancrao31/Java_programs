8ed726fd2f4dc8121c00b2ac6a1eeb10413e654e
Mohan C Rao <mohancrao31@gmail.com>
2020-04-23 13:55:13 +0530

    modified parameter

--- BarkingDog/src/com/udemycourse/Main.java
+++ BarkingDog/src/com/udemycourse/Main.java
@@ -3,7 +3,7 @@ package com.udemycourse;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(BarkingDog.shouldWakeUp(true,8));
+        System.out.println(BarkingDog.shouldWakeUp(true,9));
 
 
     }
